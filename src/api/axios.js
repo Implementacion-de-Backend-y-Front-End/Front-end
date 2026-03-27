210c1a87b25c41e6b6a0fefa4fcc35099a5b7d6e
Daniela Gonzalez <vazquezdanig64@gmail.com>
2026-03-26 22:17:21 -0600

    url

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const clienteAxios = axios.create({
-  baseURL: "http://localhost:4000/api", // La URL de tu servidor de Node
+  // 1. Prioridad: Usa la variable de entorno de Railway, si no existe, usa localhost
+  baseURL: import.meta.env.VITE_API_URL || "http://localhost:4000/api",
 });
 
 // Interceptor para enviar el token en los headers siempre
@@ -9,6 +10,7 @@ clienteAxios.interceptors.request.use(
   (config) => {
     const token = localStorage.getItem("token");
     if (token) {
+      // Asegúrate de que tu Backend use este mismo nombre: 'x-auth-token'
       config.headers["x-auth-token"] = token;
     }
     return config;
